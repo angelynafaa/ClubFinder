52f8b229bbb0f2ae5a0957026a3e777805f5c8d5
angelyna <anelyna826@gmail.com>
2020-03-25 09:52:14 -0500

    add style

--- src/script/component/app-bar.js
+++ src/script/component/app-bar.js
@@ -9,7 +9,28 @@ class AppBar extends HTMLElement {
     }
 
     render() {
-        this.shadowDOM.innerHTML = `<h2>Club Finder</h2>`;
+        this.shadowDOM.innerHTML = `
+        <style>
+        * {
+            margin=0;
+            padding=0;
+            box-sizing:border-box;
+        }
+        :host {
+            display: block; 
+            width: 100%;
+            background-color: cornflowerblue;
+            color: white;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        }
+        h2{
+            padding: 16px;
+            margin : 0px;
+        }
+        </style>
+
+        <h2>Club Finder</h2>`;
+        
     }
 }
 
